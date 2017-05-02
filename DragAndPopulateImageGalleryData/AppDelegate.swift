e72b1dad2ccacb294249bcb746cc49c81ac5808d
Sacha Moo <sacha@smoointeractive.com>
2017-05-02 15:56:52 -0700

    Initial Commit

---
+++ DragAndPopulateImageGalleryData/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DragAndPopulateImageGalleryData
+//
+//  Created by Sacha Moo on 5/2/17.
+//  Copyright © 2017 Sacha Moo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DragAndPopulateImageGalleryData/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DragAndPopulateImageGalleryData
+//
+//  Created by Sacha Moo on 5/2/17.
+//  Copyright © 2017 Sacha Moo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
