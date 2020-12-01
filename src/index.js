3456ee5a8223d9ebb85a09a3d1902ed366852a5e
Sacha Moo <sacha@smoointeractive.com>
2020-12-01 11:22:05 -0800

    Initial commit. Create project, add NavBar and add empty MathCard.

--- src/index.js
+++ src/index.js
@@ -1,32 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import SubtractionComponent from "./components/SubtractionComponent";
-// import Testcomponent from "./components/testcomponent";
-// import Index from "./components/testcomponent";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <div
-    style={{
-      margin: "auto",
-      width: "50%",
-      height: "25%",
-      // position: "absolute",
-      // left: "20%",
-      // top: "20%",
-      // transform: "translate(-20%, -20%)",
-      // display: "flex",
-      // alignItems: "center",
-      // justifyContent: "center",
-    }}
-  >
-    <SubtractionComponent />
-  </div>,
-  // <Index />,
-  // <Testcomponent />,
-  document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
